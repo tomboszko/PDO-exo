7a06353f314a18eea33fafe698eebdfcf37dd00b
Thomas Gomree <gomreethomas@gmail.com>
2023-11-14 16:28:17 +0100

    starting PDO part. 2

---
+++ index2.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!-- Exercice 1
+Créer un formulaire permettant d'ajouter un client dans la base de données. Il devra comporter les champs : nom, prénom, date de naissance, carte de fidélité (case à cocher) et numéro de carte de fidélité.
+
+A l'aide de ce formulaire, ajouter à la liste des clients Alicia Moore née le 8 septembre 1979 et ne possédant pas de carte de fidélité.
+
+Exercice 2
+Créer un formulaire permettant d'ajouter un client dans la base de données. Ajouter à ce formulaire les champs permettant de créer une carte de fidélité : numéro de carte et type de carte.
+
+Ajouter, grâce à ce formulaire, Louise Ciccone née le 16 août 1958 et possédant une carte de fidélité. Ajouter sa carte de fidélité n°7125. C'est une carte de type 2.
+
+Exercice 3
+Créer un formulaire permettant d'ajouter un spectacle. Il contiendra les champs : titre, artiste, date, type de spectacle, genre 1, genre 2, durée, heure de début.
+
+Ajouter le spectacle "I love techno" de David Guetta qui a lieu le 20 septembre 2019. C'est un concert (showTypesId : 1) de musique électronique (firstGenresId : 4) et clubbing (secondGenreId : 10) qui dure 3 heures et qui commence à 21h.
+
+Exercice 4
+Créer un formulaire comprenant les champs : nom, prénom, date de naissance, carte de fidélité (case à cocher) et numéro de carte de fidélité. Ce formulaire devra permettre de modifier un client.
+
+Dans ce formulaire, afficher les information de Gabriel Perry. Modifier sa date de naissance : il est né le 9 avril 1994.
+
+Exercice 5
+Créer un formulaire permettant de modifier un spectacle. Afficher les informations de Vestibulum accumsan. Modifier la date du spectacle : il est repoussé au 1er janvier 2017 à 21h.
+
+Exercice 6
+Créer un formulaire permettant de modifier un client. Afficher les informations du client n°5. Modifier son nom et son prénom : il s'appellera Nicolas Monteiro.
+
+Exercice 7
+Créer deux formulaires contenant tous les champs nécessaires à la création ou modification des clients. Afficher les informations des clients n°24 et 25. Les supprimer grâce à un bouton supprimer que vous aurez ajouté. (Voir image fournie)
+
+Exercice 8
+Afficher autant de formulaires que de réservations dont l'id client est 24 ou 25. Après les formulaires, ajouter un bouton supprimer et supprimer toutes ces réservations. (Voir image fournie)
+
+Exercice 9
+Afficher autant de formulaires que de billets appartenant aux réservations 24 ou 25. Après les formulaires, ajouter un bouton supprimer et supprimer tous ces billets. (Voir image fournie)    
+ -->
+
+</body>
+</html>
